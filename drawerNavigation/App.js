f9589046f8880e78c4dab11d255eb30d9dc4bfd4
João Augusto Palma Paixão <joaoaugustopp@gmail.com>
2025-09-20 13:41:28 -0300

    Finalizando o drawer navigation

--- drawerNavigation/App.js
+++ drawerNavigation/App.js
@@ -1,12 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+
+const Drawer = createDrawerNavigator();
+
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>🏠 Home</Text>
+    </View>
+  );
+}
+
+function SettingsScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>⚙️ Settings</Text>
+    </View>
+  );
+}
+
+function AccountScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>👤 Account</Text>
+    </View>
+  );
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+          <Drawer.Navigator initialRouteName='Home'>
+            <Drawer.Screen name="Home" component={HomeScreen} />
+            <Drawer.Screen name="Settings" component={SettingsScreen} />
+            <Drawer.Screen name="Account" component={AccountScreen} />
+          </Drawer.Navigator>
+        </NavigationContainer>
   );
 }
 
