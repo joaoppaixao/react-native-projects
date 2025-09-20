21996c28e458e5e0ec36983a74f5837c3cfb52ae
João Augusto Palma Paixão <joaoaugustopp@gmail.com>
2025-09-20 13:59:58 -0300

    Finalizando o stack navigation

--- stackNavigation/App.js
+++ stackNavigation/App.js
@@ -1,12 +1,49 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
+function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>🏠 Home</Text>
+      <Button title="Ir para detalhes" onPress={() => navigation.navigate('Detalhes', { produtoId: 101 })}>
+      </Button>
+    </View>
+  );
+}
+
+function DetailsScreen({ route, navigation }) {
+  const { produtoId } = route.params;
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>📄 Details</Text>
+      <Text>Id do Produto: {produtoId}</Text>
+      <Button title="Ir para Perfil" onPress={() => navigation.navigate('Perfil')}>
+      </Button>
+    </View>
+  );
+}
+
+function ProfileScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>👤 Account</Text>
+    </View>
+  );
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Home'>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Detalhes" component={DetailsScreen} />
+        <Stack.Screen name="Perfil" component={ProfileScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
